d782fe8eae2fe05b5da3333041c21f68a9126630
Kyle Kendall <kkendall33@gmail.com>
2021-05-22 00:14:52 -0600

    Referrer data model

---
+++ Sources/CarSwaddleStore/Models/Referrer+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kyle Kendall on 5/14/21.
+//
+
+import CoreData
+
+@objc(Referrer)
+public final class Referrer: NSManagedObject, NSManagedObjectFetchable {
+    
+    @NSManaged public var identifier: String
+    // Pseudo-enum for source: user, email, ad, campaign, etc
+    @NSManaged public var sourceType: String
+    // Internal metadata - id for ad campaign, email template
+    @NSManaged public var externalID: String
+    @NSManaged public var createdAt: Date
+    @NSManaged public var updatedAt: Date
+    @NSManaged public var referrerDescription: String?
+    @NSManaged public var stripeExpressAccountID: String
+    @NSManaged public var vanityID: String
+    @NSManaged public var activeCouponID: String?
+    @NSManaged public var activePayStructureID: String?
+    @NSManaged public var userID: String
+    @NSManaged public var couponID: String?
+    @NSManaged public var payStructureID: String?
+    
+}
